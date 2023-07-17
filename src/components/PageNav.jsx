b4e626eca1e4acace567c856faed332b2f87f4c8
Pier2208 <p.leroy.mtl@gmail.com>
2023-07-17 18:41:39 -0400

    (feat): add pages product, pricing and login

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,20 @@
 import { NavLink } from "react-router-dom";
-import styles from './Nav.module.css'
+import styles from "./PageNav.module.css";
+import Logo from './Logo';
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
         </li>
       </ul>
     </nav>
