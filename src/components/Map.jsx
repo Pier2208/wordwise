3bcee1209de96208aa1e9dd8053b77593851e33c
Pier2208 <p.leroy.mtl@gmail.com>
2023-07-19 16:49:05 -0400

    (feat):update map dynamically on position change or on map click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,32 +1,73 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../context/CitiesContext";
 
 export default function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  //const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
-  // const lat = searchParams.get("lat");
-  // const lng = searchParams.get("lng");
+  const { cities } = useCities();
+
+  // get lat/long in query string
+  const [searchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  // update map when lat & long are in url
+  useEffect(() => {
+    if (lat && lng) setMapPosition([lat, lng]);
+  }, [lat, lng]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
-        center={mapPosition}
-        zoom={13}
+        center={mapPosition} // center is not reactive when pos changes
+        zoom={10}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              {city.emoji} {city.cityName}
+            </Popup>
+          </Marker>
+        ))}
+        {/* dynamically change the map view with this React component to force a re-render */}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
 }
+
+// React component to dynamically change map postion
+const ChangeCenter = ({ position }) => {
+  const map = useMap();
+  map.setView(position);
+  return null;
+};
+
+// React component to detect click and get lat/long where clicked on the map
+const DetectClick = () => {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: ({ latlng }) => {
+      navigate(`form?lat=${latlng.lat}&lng=${latlng.lng}`);
+    },
+  });
+};
